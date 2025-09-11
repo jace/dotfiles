c315bf65036202b288514b75e87da5affa66c710
Kiran Jonnalagadda <kiran@zaiki.org>
2025-09-11 17:13:44 +0530

    Update .oh-my-zsh-custom/batcat.zsh Update .config/bat/config

--- dot_oh-my-zsh-custom/batcat.zsh
+++ dot_oh-my-zsh-custom/batcat.zsh
@@ -1,3 +1,7 @@
+# Older bat doesn't support `--theme(light|dark)` in config, so use ENV
+export BAT_THEME_LIGHT="Coldark-Cold"
+export BAT_THEME_DARK="Visual Studio Dark+"
+
 if (( $+commands[bat] )); then
   alias cat='bat --paging=never --style=plain'
 elif (( $+commands[batcat] )); then
